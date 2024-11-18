8dc96f7a4f4fcb2da76c8e60ee19f52bab10752e
ajsantanav <adriansantana.v@gmail.com>
2024-11-18 09:16:40 -0500

    completed rlab

--- test.js
+++ test.js
@@ -1,78 +1,9 @@
-// FizzBuzz
-//let x = 80;
 let arr = []; //stored in the first array
 let arr2 = []; //stored in the second array
 let tempArr = []; //temporary array
 const peopleData = []; //
 let cvsString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 let count = 1;
-
-function fizzBuzz (x) {
-    for (let i = 1; i<=x; i++)
-    {    
-        if(i % 3 == 0 && i % 5 == 0)
-        {
-            console.log("fizzBuzz");
-        }
-        else if(i % 5 == 0)
-        {
-            console.log("fizz");
-        }
-        else if (i % 3 == 0)
-        {
-            console.log("buzz");
-        }
-        else
-        {
-            console.log(i);
-        }        
-    }
-}
-let myMin = 1;
-let myMax = 100
-let x = randomNumber();
-function randomNumber() {
-    let num = Math.floor(Math.random() * 10);
-    return num;
-}
-
-console.log("Starting number is " + x)
-//primeNumber 
-function isPrime(x) {
-   //checks for value to not be 1 or 0
-    if( x <= 1) {
-        return false;
-    }
-    //Hard checks for values 2, 3, 5 since divided by themselves can cause a problem and not return a value
-    //|| x == 5 || x == 3 || x == 7
-    else if(x == 2 || x == 5 || x == 3 || x == 7) {
-        return true;
-    }
-    //|| x % 5 == 0 || x % 7 == 0
-    else if(x % 2 == 0 || x % 3 == 0){
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-console.log(nextPrime(x))
-function nextPrime(x)
-{
-    let num = x;
-    const nextPrime = false;
-    while(!nextPrime){
-        num++;
-        if(isPrime(num)){
-            return num;
-        }
-
-    }
-}
-
-function stringToTable(cvsString) {
-
-}
 
 function parseCsv(cvsString) {
     let word = '';
@@ -135,11 +66,13 @@ function convertToObj() {
     
 }
 
+
 parseCsv(cvsString);
 // console.log(arr);
 arraySections(arr);
 // console.log(arr2);
 convertToObj(arr2);
+
 
 peopleData.unshift({ id: "48", name: "Barry", occupation: "Runner", age: "25" });
 peopleData.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
